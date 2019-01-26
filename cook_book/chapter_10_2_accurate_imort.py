e46fe0614d62a0bf6bda01ec1c4b6cb8a043123e
Handsomer <sun_pro12580@163.com>
2019-01-26 18:24:44 +0800

    accurate import model

---
+++ cook_book/chapter_10_2_accurate_imort.py
@@ -0,0 +1,5 @@
+from chapter_10_2_accurate_imort_model import *
+
+import_func1()
+import_func2()
+   
---
+++ cook_book/chapter_10_2_accurate_imort_model.py
@@ -0,0 +1,7 @@
+def import_func1():
+    print("func1")
+
+def import_func2():
+    print("func2")
+
+__all__ = ['import_func1','import_func2']
