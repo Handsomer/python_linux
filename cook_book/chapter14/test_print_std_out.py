245bbe7eac09c830d7ebc1ffa1a344abc5d6b84a
Handsomer <sun_pro12580@163.com>
2019-12-20 14:56:01 +0800

    添加部分注视

--- cook_book/chapter14/test_print_std_out.py
+++ cook_book/chapter14/test_print_std_out.py
@@ -7,6 +7,7 @@ class TestURLPrint(TestCase):
     def test_url_gets_to_stdout(self):
         protocol, host, domain = 'http', 'www', 'example.com'
         expected_url = '{}://{}.{}\n'.format(protocol, host, domain)
+        #patch 可生成一个对象作为sys.stdout的替身 --- unittest.mock.path
         with patch('sys.stdout',  new = StringIO()) as fake_out:
             mymodule.urlPrint(protocol, host, domain)
             self.assertEqual(fake_out.getvalue(), expected_url)
